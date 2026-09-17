caac7dfc563eccf1bc4ab37756306bb077b16e41
datnguyen.cr <datnguyen.cr@gmail.com>
2026-09-17 22:58:54 +0700

    updated

--- update_readme.dart
+++ update_readme.dart
@@ -18,7 +18,7 @@ void main() {
     title: '📱 Mobile Apps (${apps.length})',
     items: apps.map<String>((item) {
       return _link(
-        '<img src="${item['icon']}=s32" width="16" height="16" /> ${item['name']}',
+        '<img src="${item['icon']}=s32" width="24" height="24" /> ${item['name']}',
         'https://play.google.com/store/apps/details?id=${item['id']}',
       );
     }).toList(),
@@ -29,7 +29,7 @@ void main() {
     title: '🌐 Web Apps (${webs.length})',
 
     items: webs.map<String>((item) {
-      return _link('<img src="${item['icon']}=s32" width="16" height="16" /> ${item['name']}', item['url']);
+      return _link('<img src="${item['icon']}=s32" width="24" height="24" /> ${item['name']}', item['url']);
     }).toList(),
   );
 
@@ -37,7 +37,7 @@ void main() {
     buffer: buffer,
     title: '🧩 Chrome Extensions (${chrome.length})',
     items: chrome.map<String>((item) {
-      return _link('<img src="${item['icon']}=s32" width="16" height="16" /> ${item['name']}', item['url']);
+      return _link('<img src="${item['icon']}=s32" width="24" height="24" /> ${item['name']}', item['url']);
     }).toList(),
   );
 
